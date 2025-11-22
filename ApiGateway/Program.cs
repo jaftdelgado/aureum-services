cc946ded2d8c7cf7ca3bc9adbc888d6cfeb03729
Cesar Eduardo Ceron Martinez <zS22013637@estudiantes.uv.mx>
2025-11-21 20:02:01 -0600

    feat: ApiGateway structure

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,41 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+var supabaseSecret = Environment.GetEnvironmentVariable("SUPABASE_JWT_SECRET") ?? "CLAVE_TEMPORAL_PARA_LOCAL_123456789";
+var bytes = Encoding.UTF8.GetBytes(supabaseSecret);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer("SupabaseAuth", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(bytes),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization(); 
+
+await app.UseOcelot();  
+
+app.Run();
