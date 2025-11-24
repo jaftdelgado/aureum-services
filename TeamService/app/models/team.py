a36c4026ac8982d7d801efabbd6c727d4464cd26
CesarEduardoCeronM <162655804+CesarEduardoCeronM@users.noreply.github.com>
2025-11-23 18:25:50 -0600

    Update team.py

--- TeamService/app/models/team.py
+++ TeamService/app/models/team.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
