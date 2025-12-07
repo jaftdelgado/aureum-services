f1ddd5477282bdac29c04d87f22ec9d7e565b376
FideMontemira <161769036+FideMontemira@users.noreply.github.com>
2025-12-06 18:19:45 -0600

    Update PortfolioTransactionDto.cs

--- PortfolioService/Dtos/PortfolioTransactionDto.cs
+++ PortfolioService/Dtos/PortfolioTransactionDto.cs
@@ -1,15 +1,20 @@
-﻿namespace PortfolioService.Dtos
- using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+namespace PortfolioService.Dtos
+ 
 {
     public class PortfolioTransactionDto
     {
-        [JsonPropertyName("userPublicId")]
+       [JsonPropertyName("userPublicId")]
+        [JsonProperty("userPublicId")] 
         public Guid UserId { get; set; }
 
         [JsonPropertyName("teamPublicId")]
+        [JsonProperty("teamPublicId")]
         public Guid TeamId { get; set; }
 
         [JsonPropertyName("assetPublicId")]
+        [JsonProperty("assetPublicId")]
         public Guid AssetId { get; set; }
        
         public double Quantity { get; set; }
