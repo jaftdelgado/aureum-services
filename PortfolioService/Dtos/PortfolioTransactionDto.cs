ae3370f387dd7a49342307a237674f40bfcd5f71
FideMontemira <161769036+FideMontemira@users.noreply.github.com>
2025-12-06 18:12:02 -0600

    Add JsonPropertyName attributes to DTO properties

--- PortfolioService/Dtos/PortfolioTransactionDto.cs
+++ PortfolioService/Dtos/PortfolioTransactionDto.cs
@@ -1,14 +1,19 @@
 ﻿namespace PortfolioService.Dtos
+ using System.Text.Json.Serialization;
 {
     public class PortfolioTransactionDto
     {
+        [JsonPropertyName("userPublicId")]
         public Guid UserId { get; set; }
-        public Guid TeamId { get; set; }
-        public Guid AssetId { get; set; }
 
+        [JsonPropertyName("teamPublicId")]
+        public Guid TeamId { get; set; }
+
+        [JsonPropertyName("assetPublicId")]
+        public Guid AssetId { get; set; }
        
         public double Quantity { get; set; }
         public decimal Price { get; set; } 
-        public bool IsBuy { get; set; }
+        public bool IsBuy { get; set; } = true;
     }
 }
